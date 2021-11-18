a5b8277411c31f831c24eb0f6725de0ac7a56e98
TitikUniverse <semyon.titov.203@mail.ru>
2021-11-18 19:24:29 +0300

    Верстал login страницу

---
+++ src/views/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="parents">
+      <form class="main">
+          <div class="form-item">
+              <input type="text">
+          </div>
+
+          <div class="form-item">
+              <input type="text">
+          </div>
+          
+          <div class="form-item">
+              <button>Войти</button>
+          </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+
+.parents{
+    width: 100%;
+    margin: 0%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.main {
+  width: 430px;
+  height: 520px;
+  background-color: rgb(50, 100, 125);
+  margin-top: 150px;
+  border-radius: 7px;
+  padding: 40px 60px;
+}
+
+.form-item {
+    width: 100%;
+    height: 100px;
+}
+</style>
